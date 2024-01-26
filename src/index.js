99395fc9a95f0669754486fb74a8f9bc4e44ca8c
sutti <suttipong.ta@rmuti.ac.th>
2024-01-26 14:04:01 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
-require('dotenv').config()
-const express = require("express")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const errorHandler = require("./middlewares/error")
-const notFoundHandler = require("./middlewares/notFound")
-const createError = require('./utils/createError')
+const errorHandler = require("./middlewares/error");
+const notFoundHandler = require("./middlewares/notFound");
+const createError = require("./utils/createError");
 
-const authRoute = require("./routes/auth-route")
-const productRoute = require("./routes/product-route")
+const authRoute = require("./routes/auth-route");
+const productRoute = require("./routes/product-route");
+const adminRoute = require("./routes/admin-route")
+const authenticate = require("./middlewares/authenticate");
+const admin = require('./middlewares/admin');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/auth", authRoute)
-app.use("/product", productRoute)
+app.use("/auth", authRoute);
+app.use("/product", productRoute);
+// multer อ่าน file -> formData
+// เขียน file ลงบนโปรเจค
+// upload ขึ้น clound
+// Clound -> url
+// เก็บ url เช้า DB
+// ลบรูปบน server
+app.use('/admin', authenticate, admin, adminRoute);
 
-app.use(errorHandler)
-app.use("*", notFoundHandler)
+app.use(errorHandler);
+app.use("*", notFoundHandler);
 
-const port = process.env.PORT
+const port = process.env.PORT;
 app.listen(port, () => {
-    console.log("Server run in port" + " " + port)
-})
+  console.log("Server run in port" + " " + port);
+});
